535cef6dddf14424b713625d5b70ffb27a3172a4
CarlaDupont <carla.dupont@ynov.com>
2026-03-19 11:21:46 +0100

    fix/config api gateway

---
+++ api-gateway/src/main/java/org/example/apigateway/GatewayRoutesConfig.java
@@ -0,0 +1,23 @@
+package org.example.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> roomServiceRoute() {
+        return route("room-service-route")
+                .route(RequestPredicates.path("/rooms/**"), http())
+                .filter(lb("room-service"))
+                .build();
+    }
+}
